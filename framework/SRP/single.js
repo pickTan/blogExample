53159a9aeb373dca3f796c1b75516e5e6353d5b6
fynnpeng <514625181@qq.com>
2020-07-29 20:15:38 +0800

    提交博客的例子代码

---
+++ framework/SRP/single.js
@@ -0,0 +1,42 @@
+/**
+ * 创建登陆框的惰性单例(只有使用时才加载)模式
+ * 职责 1. 创建登陆的DIV  2.实例化
+ * 坏处：1.以后其他的
+ * @returns {function(): HTMLDivElement}
+ */
+function createLoginLayer(){
+    var div;
+    return function () {
+        if(!div){
+            div =  document.createElement('div');
+            div.innerHTML = '我是登陆框';
+            div.style.display='none';
+            document.body.append(div)
+        }
+        return div;
+    }
+}
+
+var loginLayer = createLoginLayer()()
+
+//进行职责拆分
+//拆分成 1.单例生成器  2.登陆层
+
+//单例模式是典型的单一职责的
+function getSingle(fn) {
+    var res;
+    return function () {
+        return res || (res = fn.call(this,arguments))
+    }
+}
+
+function loginLayer() {
+   var div =  document.createElement('div');
+    div.innerHTML = '我是登陆框';
+    div.style.display='none';
+    document.body.append(div)
+    return div;
+}
+
+var loginLayer = getSingle(loginLayer)()
+
